eaa37f033950ba4c7a3ceb01531c894d3b96880a
김민수 <gimminsu@gimminsuui-MacBookPro.local>
2021-07-16 15:23:43 +0900

    설정 조금 바꿈

--- app.js
+++ app.js
@@ -2,34 +2,34 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const http = require("http");
-
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+app.use(
+  cors({
+    origin: "*",
+    Methods: ["POST", "GET", "OPTIONS"],
+    credentials: true,
+    header: "*",
+    optionsSuccessStatus: 200,
+  })
+);
 
 const passport = require("passport");
 const passportConfig = require("./controllers/passport");
 app.use(passport.initialize());
 passportConfig();
 
-app.use(express.urlencoded({ extended: false }));
-
-app.use(cookieParser());
-app.use(express.json());
 const port = 8080;
 
 app.get("/", (req, res) => {
   res.send("hello world");
 });
-
-app.use(
-  cors({
-    origin: true,
-    Methods: ["POST", "GET", "OPTIONS"],
-    credentials: true,
-    // headers: {
-    //   "Content-Type": "application/json",
-    // },
-  })
-);
 
 const userRouter = require("./routes/user");
 app.use("/user", userRouter);
